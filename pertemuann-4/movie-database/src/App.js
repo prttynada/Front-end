cccb758a3c4079f291033162e1bfa013e48f900d
prttynada <prettinada@gmail.com>
2022-03-16 15:21:11 +0700

    pertemuan-4

---
+++ pertemuann-4/movie-database/src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+import Header from "./components/Header.js";
+import Main from "./components/Main.js";
+import Footer from "./components/Footer.js";
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ pertemuann-4/movie-database/src/components/Header.js
@@ -0,0 +1,18 @@
+/**
+ * Membuat Component Header.
+ * Menampilkan Navigasi
+ */
+
+ function Header() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
